01d06544c70e61aaee8f18e0cea9a569bedf1eae
Thaina <thainayu@gmail.com>
2022-07-31 13:28:39 +0700

    fix singleton and change material for webgl

--- Assets/Marching_Cubes/Scripts/Helper/Singleton.cs
+++ Assets/Marching_Cubes/Scripts/Helper/Singleton.cs
@@ -49,7 +49,7 @@ public abstract class Singleton<T> : MonoBehaviour where T : Component
             instance = this as T;
             //DontDestroyOnLoad(gameObject);
         }
-        else
+        else if(instance != this)
         {
             Destroy(gameObject);
         }
